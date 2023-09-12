673e672298ff22507ae8de7e1853f9d873c1d9ec
elijah <popoolakejiah@gmail.com>
2023-09-12 21:22:49 +0100

    update: tailwind update

--- src/components/bottom-input/BottomInput.tsx
+++ src/components/bottom-input/BottomInput.tsx
@@ -7,14 +7,14 @@ function BottomInput() {
 
   return (
     <div className="bg-white p-4 sm:hidden fixed z-10 bottom-0 mx-auto left-0 right-0">
-      <div className="relative">
-        <input
-          onFocus={() => modalControlContext.setIsAddOpen(true)}
-          placeholder="Input task"
-          className="border w-full border-gray-300 rounded-lg py-2 pl-2 pr-9 placeholder:text-gray-500 placeholder:font-medium"
-        />
+      <div
+        className="relative"
+        onClick={() => modalControlContext.setIsAddOpen(true)}
+      >
+        <div className="border w-full border-gray-300 rounded-lg py-2 pl-2 pr-9">
+          <p className="text-gray-500 font-medium">Input task</p>
+        </div>
         <BsFillMicFill
-          onClick={() => modalControlContext.setIsAddOpen(true)}
           size={18}
           className="text-blue-600 absolute top-3 right-3"
         />
